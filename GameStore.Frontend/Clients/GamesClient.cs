a52d7718cedb4b1946c685f2a6d88b579e14f271
Szymon Miękus <miekusoft@gmail.com>
2024-07-05 01:37:22 +0200

    Add genres into edit form

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -6,22 +6,10 @@ public class GamesClient
 {
 	private readonly List<GameSummary> _games =
 	[
-		new()
-		{
-			Id = 1, Name = "Game 1", Genre = "Action", Price = 10.0M, ReleaseDate = new DateOnly(2021, 1, 1)
-		},
-		new()
-		{
-			Id = 2, Name = "Game 2", Genre = "Sport", Price = 21.0M, ReleaseDate = new DateOnly(1992, 1, 1)
-		},
-		new ()
-		{
-			Id = 3, Name = "Game 3", Genre = "RPG", Price = 15.0M, ReleaseDate = new DateOnly(2000, 1, 1)
-		},
-		new ()
-		{
-			Id = 4, Name = "Game 4", Genre = "Action", Price = 30.0M, ReleaseDate = new DateOnly(2010, 1, 1)
-		},
+		new() { Id = 1, Name = "Game 1", Genre = "Action", Price = 10.0M, ReleaseDate = new DateOnly(2021, 1, 1) },
+		new() { Id = 2, Name = "Game 2", Genre = "Sport", Price = 21.0M, ReleaseDate = new DateOnly(1992, 1, 1) },
+		new() { Id = 3, Name = "Game 3", Genre = "RPG", Price = 15.0M, ReleaseDate = new DateOnly(2000, 1, 1) },
+		new() { Id = 4, Name = "Game 4", Genre = "Action", Price = 30.0M, ReleaseDate = new DateOnly(2010, 1, 1) },
 	];
 
 	public GameSummary[] GetGames() => [.. _games];
---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,15 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+	private readonly Genre[] _genres =
+	[
+		new() { Id = 1, Name = "Action" },
+		new() { Id = 2, Name = "Adventure" },
+		new() { Id = 3, Name = "RPG" },
+	];
+
+	public Genre[] GetGenres() => [.. _genres];
+}
