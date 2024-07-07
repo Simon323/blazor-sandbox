75c736454630b3dacf817535080b5346a6a47637
Szymon Miękus <miekusoft@gmail.com>
2024-07-07 15:11:40 +0200

    Add seed data

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -8,5 +8,18 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
 {
     public DbSet<Game> Games => Set<Game>();
 
-    public DbSet<Genre> Genres => Set<Genre>(); 
+    public DbSet<Genre> Genres => Set<Genre>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder
+			.Entity<Genre>()
+			.HasData(
+				new { Id = 1, Name = "Action" },
+				new { Id = 2, Name = "Adventure" },
+				new { Id = 3, Name = "RPG" },
+				new { Id = 4, Name = "Simulation" },
+				new { Id = 5, Name = "Strategy" }
+			);
+	}
 }
